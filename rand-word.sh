cb85f55c248db16cfe0c2964e489834c24bac8a8
Tristan Chase <tristan.m.chase@gmail.com>
2025-01-24 15:26:49 +0000

    feat: initial commit

---
+++ rand-word.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+
+#-----------------------------------
+# Usage Section
+
+#<usage>
+#//Usage: rand-word [ {-d|--debug} ] [ {-h|--help} | <options>] [<arguments>]
+#//Description: Picks a random word or list of random words
+#//Examples: rand-word foo; rand-word --debug bar
+#//Options:
+#//	-d --debug	Enable debug mode
+#//	-h --help	Display this help message
+#</usage>
+
+#<created>
+# Created: 2025-01-24T15:02:58+00:00
+# Tristan M. Chase <tristan.m.chase@gmail.com>
+#</created>
+
+#<depends>
+# Depends on:
+#  list
+#  of
+#  dependencies
+#</depends>
+
+#-----------------------------------
+# TODO Section
+
+#<todo>
+# TODO
+# * Insert script
+# * Clean up stray ;'s
+# * Modify command substitution to "$(this_style)"
+# * Rename function_name() to function __function_name__ /\w+\(\)
+# * Rename $variables to "${_variables}" /\$\w+/s+1 @v vEl,{n
+# * Check that _variable="variable definition" (make sure it's in quotes)
+# * Update usage, description, and options section
+# * Update dependencies section
+
+# DONE
+
+#</todo>
+
+#-----------------------------------
+# License Section
+
+#<license>
+# Put license here
+#</license>
+
+#-----------------------------------
+# Runtime Section
+
+#<main>
+# Initialize variables
+#_temp="file.$$"
+
+# List of temp files to clean up on exit (put last)
+#_tempfiles=("${_temp}")
+
+# Put main script here
+function __main_script__ {
+
+_word_file="/usr/share/dict/words"
+
+# shuf -n1 "${_word_file}"
+awk -v word_length="${1:-}" 'length ($0) == word_length' "${_word_file}" | less -F
+
+# Check to see if $_word_file is installed
+# Check that word_length is a number 0 < word_length < wc -L +1
+# Options:
+# 	Pick a random word
+# 	Pick a random word of length $word_length
+# 	Generate a list of words of length $word_length
+#	Generate a list of length $list_length
+
+} #end __main_script__
+#</main>
+
+#-----------------------------------
+# Local functions
+
+#<functions>
+function __local_cleanup__ {
+	:
+}
+#</functions>
+
+#-----------------------------------
+# Source helper functions
+for _helper_file in functions colors git-prompt; do
+	if [[ ! -e ${HOME}/."${_helper_file}".sh ]]; then
+		printf "%b\n" "Downloading missing script file "${_helper_file}".sh..."
+		sleep 1
+		wget -nv -P ${HOME} https://raw.githubusercontent.com/tristanchase/dotfiles/main/"${_helper_file}".sh
+		mv ${HOME}/"${_helper_file}".sh ${HOME}/."${_helper_file}".sh
+	fi
+done
+
+source ${HOME}/.functions.sh
+
+#-----------------------------------
+# Get some basic options
+# TODO Make this more robust
+#<options>
+if [[ "${1:-}" =~ (-d|--debug) ]]; then
+	__debugger__
+elif [[ "${1:-}" =~ (-h|--help) ]]; then
+	__usage__
+fi
+#</options>
+
+#-----------------------------------
+# Bash settings
+# Same as set -euE -o pipefail
+#<settings>
+set -o errexit
+set -o nounset
+set -o errtrace
+set -o pipefail
+IFS=$'\n\t'
+#</settings>
+
+#-----------------------------------
+# Main Script Wrapper
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
+	trap __traperr__ ERR
+	trap __ctrl_c__ INT
+	trap __cleanup__ EXIT
+
+	__main_script__
+
+
+fi
+
+exit 0
